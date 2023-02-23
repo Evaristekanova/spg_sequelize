8ea6ec0fe324585484029553b090c67bf6123cbf
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-02-23 09:45:30 +0200

    Branch(main)[configuring babel and changing syntax to ES6]

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import userRoutes from './routes/userRoutes'
+
 const app = express()
 app.use(express.json())
-app.get('/', (req, res) => { 
-    console.log('hello world');
-    res.send('Hello World!')
-})
+
 app.use('/api', userRoutes)
 const port = 8080
 app.listen(port, () => {
